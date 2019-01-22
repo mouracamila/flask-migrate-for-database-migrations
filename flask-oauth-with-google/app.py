a6c6d6b51efeb870d636f37052e1cce45cc01fd6
Camila Moura <camilapaleo@gmail.com>
2019-01-22 11:47:43 +0100

    wlcome ok

--- flask-oauth-with-google/app.py
+++ flask-oauth-with-google/app.py
@@ -1,6 +1,10 @@
 import os
-os.environ['AOUTHLIB_INSECURE_TRANSPORT'] = '1'
-os.environ['OAUTHLIB_RELEX_TOKEN_SCOPE'] = '1'
+#os.environ['AOUTHLIB_INSECURE_TRANSPORT'] = 1
+#os.environ['OAUTHLIB_RELEX_TOKEN_SCOPE'] = 1
+
+## Loggin system
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 #########
 from flask import Flask,redirect,url_for,render_template
@@ -9,8 +13,16 @@ from flask_dance.contrib.google import make_google_blueprint,google
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 
-blueprint = make_google_blueprint(client_id='824848467259-fluj67834hk5ndlbecpbpepbaeuojl44.apps.googleusercontent.com',
-client_secret='qkGhC9oOtxBtCBNICNieQiEl',offline=True,scope=['profile','email'])
+blueprint = make_google_blueprint(
+    client_id='824848467259-fluj67834hk5ndlbecpbpepbaeuojl44.apps.googleusercontent.com',
+    client_secret='6s0t58w0QyD5z3fyjbD7JZgA',
+    offline=True,
+    scope=[
+        'https://www.googleapis.com/auth/userinfo.profile',
+        'https://www.googleapis.com/auth/userinfo.email'
+        #'profile', 'email'
+    ]
+)
 
 app.register_blueprint(blueprint,url_prefix='/login')
 
@@ -31,11 +43,10 @@ def welcome():
 def login():
     if not google.authorized:
         return render_template(url_for('google.login'))
-    resp = google.get('/oauth2/v2/userinfo')
-    assert resp.ok, resp.text
-    email = resp.json()['email']
 
-    return render_template('welcome.html',email=email)
+    return redirect(url_for('welcome'))
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(ssl_context='adhoc')
+    #app.run()
